a75222a06692e4319ce0d1ac8c94c85614a550ad
Rosangela <Rosangela@DESKTOP-NF23MHI>
2020-04-05 20:14:43 -0300

    feat: Cria a classe Profissional Além dos atributos

---
+++ src/main/java/br/lgomesd/contratecerto/Profissional.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.lgomesd.contratecerto;
+
+/**
+ *
+ * @author Rosangela
+ */
+public class Profissional {
+    
+       String nomecompleto = "";
+       String Endereco= "";
+       String email;
+       int CPF = 0;
+       int telefone;
+       int numeroCertificado = 0;
+    
+}
