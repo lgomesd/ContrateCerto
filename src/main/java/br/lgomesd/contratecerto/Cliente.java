2579d5149ff0e38d880ff90d9d156e92ec8c0d27
Adriano Animus <Adriano Animus@AdrianoAnimus>
2020-04-05 17:10:04 -0300

    feat: Efetua encapsulamento da classe

--- src/main/java/br/lgomesd/contratecerto/Cliente.java
+++ src/main/java/br/lgomesd/contratecerto/Cliente.java
@@ -17,6 +17,40 @@ public class Cliente {
     String endereço;
     Date dataNascimento;
     int cpf;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    
+    
     
     
 }
