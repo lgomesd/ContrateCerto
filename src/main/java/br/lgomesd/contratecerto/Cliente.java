7658910e63ca7e2bffd139eb3d6004a406fd7353
Adriano Animus <Adriano Animus@AdrianoAnimus>
2020-04-05 16:56:53 -0300

    feat: Cria projeto Leilao online feat: Cria classe Cliente

---
+++ src/main/java/br/lgomesd/contratecerto/Cliente.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.lgomesd.contratecerto;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Adriano Animus
+ */
+public class Cliente {
+    
+    private String nome;
+    private String endereço;
+    private Date dataNascimento;
+    private int cpf;
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + this.cpf;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (this.cpf != other.cpf) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nome=" + nome + ", endere\u00e7o=" + endereço + ", dataNascimento=" + dataNascimento + ", cpf=" + cpf + '}';
+    }
+    
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the endereço
+     */
+    public String getEndereço() {
+        return endereço;
+    }
+
+    /**
+     * @param endereço the endereço to set
+     */
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    /**
+     * @return the dataNascimento
+     */
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    /**
+     * @param dataNascimento the dataNascimento to set
+     */
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public int getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    
+    
+    
+}
