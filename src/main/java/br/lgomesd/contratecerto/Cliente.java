2fc460b5604f6a58ae123cdf14f63f37e744b894
Adriano Animus <Adriano Animus@AdrianoAnimus>
2020-04-05 19:32:32 -0300

    feat:Cria classe Cliente

---
+++ src/main/java/br/lgomesd/contratecerto/Cliente.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.lgomesd.contratecerto;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Adriano Animus
+ */
+public class Cliente {
+    
+    String nome;
+    String endereco;
+    int cpf;
+    Date dataNascimento;
+    
+}
