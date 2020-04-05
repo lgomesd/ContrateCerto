73976b2b6f8b242f86ceb6432861fe04dc5ed48f
Adriano Animus <Adriano Animus@AdrianoAnimus>
2020-04-05 19:37:32 -0300

    feat:Cria classe Cliente feat: Realiza encapsulamento

--- src/main/java/br/lgomesd/contratecerto/Cliente.java
+++ src/main/java/br/lgomesd/contratecerto/Cliente.java
@@ -17,5 +17,39 @@ public class Cliente {
     String endereco;
     int cpf;
     Date dataNascimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    
+    
     
 }
