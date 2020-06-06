1f896c0c401ac1fb0221f576066c64d8a898b03e
Rosangela <Rosangela@DESKTOP-NF23MHI>
2020-06-06 13:41:12 -0300

    feat: Insere metodos cadastrar e excluir Recomendacões

---
+++ src/main/java/br/lgomesd/contratecerto/Recomendacao.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.lgomesd.contratecerto;
+
+/**
+ *
+ * @author Rosangela
+ */
+public class Recomendacao {
+    
+     private int Avaliação = 0;
+     private String Depoimentos;
+
+     
+    public boolean cadastrar(String Depoimentos, int Avaliação){
+           return false;
+       }
+     
+      public boolean Excluir(String Depoimentos, int Avaliação){
+        if(Depoimentos.isEmpty()){
+            return false;
+        }
+        else {
+              System.out.println("Depoimentos" + Avaliação + " excluída com sucesso!");
+            return true;
+        }        
+        
+    }  
+      
+          @Override
+    public String toString() {
+        return "Recomendacao{" + "Avalia\u00e7\u00e3o=" + Avaliação + ", Depoimentos=" + Depoimentos + '}';
+    }
+    
+
+    /**
+     * @return the Avaliação
+     */
+    public int getAvaliação() {
+        return Avaliação;
+    }
+
+    /**
+     * @param Avaliação the Avaliação to set
+     */
+    public void setAvaliação(int Avaliação) {
+        this.Avaliação = Avaliação;
+    }
+
+    /**
+     * @return the Depoimentos
+     */
+    public String getDepoimentos() {
+        return Depoimentos;
+    }
+
+    /**
+     * @param Depoimentos the Depoimentos to set
+     */
+    public void setDepoimentos(String Depoimentos) {
+        this.Depoimentos = Depoimentos;
+    }
+
+
+    
+ } 
