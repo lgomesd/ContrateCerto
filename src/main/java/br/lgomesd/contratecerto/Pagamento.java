1345e174a3492750a452f3714e46552c8002aaa9
Rosangela <Rosangela@DESKTOP-NF23MHI>
2020-06-07 11:58:12 -0300

    feat: associa com a classe profissional

---
+++ src/main/java/br/lgomesd/contratecerto/Pagamento.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.lgomesd.contratecerto;
+
+/**
+ *
+ * @author Rosangela
+ */
+public class Pagamento {
+    
+  //API: https://developercielo.github.io/manual/cielo-ecommerce 
+        
+        
+}
